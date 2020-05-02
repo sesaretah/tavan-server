a8eda4e174a59f01643ecb32d83f9004752253c6
sesaretah <hs.shafiei@gmail.com>
2020-05-02 16:38:38 +0430

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  Rails.application.routes.default_url_options[:host] = 'http://194.225.14.55'
+  Rails.application.routes.default_url_options[:host] = 'https://tavan.ut.ac.ir'
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
