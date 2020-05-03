1a8766c1830fa60371847ebebd4a44e71c27566c
sesaretah <hs.shafiei@gmail.com>
2020-05-03 19:12:09 +0430

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
         response = open('https://auth.ut.ac.ir:8443/cas/serviceValidate?service=https%3A%2F%2Ftavan.ut.ac.ir%2Fusers%2Fservice&ticket='+params[:ticket]).read
         result = Hash.from_xml(response.gsub("\n", ""))
         Rails.logger.info result
+        if !result['serviceResponse']['authenticationSuccess'].blank?
+            redirect_to('https://tavan.ut.ac.ir/app.html#!/login/'+result['serviceResponse']['authenticationSuccess'])
+        end
       end
     
 end
