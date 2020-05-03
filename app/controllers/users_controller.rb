c5e3603c4c323659095101ae204898b1c730a172
sesaretah <hs.shafiei@gmail.com>
2020-05-03 19:26:28 +0430

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,15 @@ class UsersController < ApplicationController
         result = Hash.from_xml(response.gsub("\n", ""))
         Rails.logger.info result
         if !result['serviceResponse']['authenticationSuccess'].blank?
-            redirect_to('https://tavan.ut.ac.ir/app.html#!/login/'+result['serviceResponse']['authenticationSuccess']['user'])
+            utid = result['serviceResponse']['authenticationSuccess']['user']
+            user = User.find_by_email(utid + '@ut.ac.ir')
+            if user.blank?
+                password = SecureRandom.hex(6)
+                user = User.create(email: utid+ '@ut.ac.ir', password: password, password_confirmation: password, last_login: DateTime.now)            
+            end
+            redirect_to('https://tavan.ut.ac.ir/app.html#!/login_jwt/'+JWTWrapper.encode({ user_id: user.id }))
+        else
+            redirect_to('https://tavan.ut.ac.ir/app.html#!/login_error/')
         end
       end
     
