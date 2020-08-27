9194be7adc40745dff02233acba328aa909af78c
sesaretah <hs.shafiei@gmail.com>
2020-08-27 11:59:10 +0430

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     def service
-        response = open('https://auth.ut.ac.ir:8443/cas/serviceValidate?service=https%3A%2F%2Ftavan.ut.ac.ir%2Fusers%2Fservice&ticket='+params[:ticket]).read
+        response = open('https://auth.ut.ac.ir:8443/cas/serviceValidate?service=https%3A%2F%2Ftavan.ut.ac.ir%2Fusers%2Fservice&ticket='+params[:ticket], {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
         result = Hash.from_xml(response.gsub("\n", ""))
         Rails.logger.info result
         if !result['serviceResponse']['authenticationSuccess'].blank?
