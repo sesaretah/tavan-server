b48c6e1625c258c2eb39afdda6c6d4b9a8b077bf
sesaretah <hs.shafiei@gmail.com>
2020-11-02 20:07:11 +0330

    1

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -4,7 +4,7 @@ class ActivitySerializer < ActiveModel::Serializer
     def series   
       #Rails.logger.info '********'
       #Rails.logger.info Visit.where(visitable_type: object.class.name, visitable_id: object.id).group('created_at::date').count
-      visits =  Visit.where('visitable_type = ? AND visitable_id = ? AND  created_at > ? ', object.class.name, object.id, 20.days.ago).group('created_at::date').count
+      visits =  Visit.where('visitable_type = ? AND visitable_id = ? AND  created_at > ? ', object.class.name, object.id, 100.days.ago).group('created_at::date').count
       #if visits > 0
         return {title: object.title, data: visits}
      # end
