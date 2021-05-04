b7baefb81b6464716dc98a21bd14338a97749220
sesaretah <hs.shafiei@gmail.com>
2021-05-04 18:29:21 +0430

    1

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -2,20 +2,23 @@ class NotificationSerializer < ActiveModel::Serializer
   cache key: "notification_serializer", expires_in: 3.hours
   attributes :id, :profile, :target_type, :target_id, :notification_text
 
-  
   def profile
-    ProfileSerializer.new(object.user.profile).as_json
+    if !object.user.blank? && !object.user.profile.blank?
+      ProfileSerializer.new(object.user.profile).as_json
+    end
   end
 
   def target_type
-    object.notifiable_type.tableize 
+    object.notifiable_type.tableize
   end
 
   def target_id
     object.notifiable_id
   end
 
-  def notification_text 
-    object.fcm_text(nil, object.notification_type, object.user.profile.fullname, object.notifiable.title, object.custom_text)
+  def notification_text
+    if !object.notifiable.blank? && !object.user.blank? && !object.user.profile.blank?
+      object.fcm_text(nil, object.notification_type, object.user.profile.fullname, object.notifiable.title, object.custom_text)
+    end
   end
 end
